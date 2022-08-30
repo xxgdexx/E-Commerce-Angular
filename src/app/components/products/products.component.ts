93bacc3911366aaa6106d5bf7abf304dd7a3d2c5
xxgdexx <wilmergabrielnegronvelasquez@gmail.com>
2022-08-30 09:20:03 -0500

    Creación de proyecto Angular

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product } from '../../models/product.model';
+import { Product , CreateProductDTO, UpdateProductDTO } from '../../models/product.model';
 
 import { StoreService } from '../../services/store.service';
 import { ProductsService } from '../../services/products.service';
@@ -8,13 +8,24 @@ import { ProductsService } from '../../services/products.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-
   myShoppingCart: Product[] = [];
   total = 0;
   products: Product[] = [];
+  showProductDetail = false;
+  productChosen: Product = {
+    id: '',
+    price: 0,
+    images: [],
+    title: '',
+    category: {
+      id: '',
+      name: '',
+    },
+    description: '',
+  };
 
   constructor(
     private storeService: StoreService,
@@ -24,8 +35,7 @@ export class ProductsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.productsService.getAllProducts()
-    .subscribe(data => {
+    this.productsService.getAllProducts().subscribe((data) => {
       this.products = data;
     });
   }
@@ -34,5 +44,43 @@ export class ProductsComponent implements OnInit {
     this.storeService.addProduct(product);
     this.total = this.storeService.getTotal();
   }
+  toggleProductDetail() {
+    this.showProductDetail = !this.showProductDetail;
+  }
 
+  onShowDetail(id: string) {
+    this.productsService.getProduct(id).subscribe((data) => {
+      this.toggleProductDetail();
+      this.productChosen = data;
+      //  console.log('product', data);
+    });
+  }
+  createNewProduct() {
+    const product: CreateProductDTO = {
+      title: 'Nuevo',
+      description: 'desc',
+      images: [''],
+      price: 1000,
+      categoryId: 2,
+    }
+    this.productsService.create(product)
+    .subscribe(data => {
+      //console.log('Created',data);
+      this.products.unshift(data);
+    });
+  }
+
+  updateProduct(){
+    const changes : UpdateProductDTO = {
+      title:'Change title' ,
+    }
+    const id = this.productChosen.id;
+    this.productsService.update(id,changes)
+    .subscribe(data =>{
+    //  console.log('updated', data);
+    const productIndex = this.products.findIndex(item => item.id === this.productChosen.id);
+    this.products[productIndex] = data;
+    });
+  }
+  
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Product } from './../models/product.model';
+import { Product , CreateProductDTO , UpdateProductDTO} from './../models/product.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
+  private apiURL = 'https://young-sands-07814.herokuapp.com/api/products';
+
   constructor(
     private http: HttpClient
   ) { }
 
   getAllProducts() {
-    return this.http.get<Product[]>('https://fakestoreapi.com/products');
+    return this.http.get<Product[]>(this.apiURL);
   }
+
+  getProduct(id: string){
+    return this.http.get<Product>(`${this.apiURL}/${id}`);
+  }
+
+  create(dto: CreateProductDTO){
+    return this.http.post<Product>(this.apiURL, dto);
+  }
+  //PUT = Todo el objeto  or PATCH una parte del objeto
+  update(id:string , dto: UpdateProductDTO){
+    return this.http.put<Product>(`${this.apiURL}/${id}`, dto);
+  }
+
 }
